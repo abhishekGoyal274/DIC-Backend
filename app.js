212948a354214f169284930d108fb1c2e1823956
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-07-02 15:23:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var backendHtml =
         <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">\
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">\
         <link rel="icon" type="image/x-icon" href="https://upload.wikimedia.org/wikipedia/en/9/94/UIET_logo.png">\
-        <title>API PuMeet</title>\
+        <title>API DIC-WEB-DEV</title>\
             <body style="background-color:black; margin:0px; padding:0px; font-family: Josefin Sans, sans-serif;;">\
                 <div style="display: flex;\
                     justify-content: center;\
