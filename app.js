baeb4595279cb74345d8c996d16613f3314dd137
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-07-02 11:46:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/uploads", express.static("uploads"));
+app.use("/verification", require("./routes/veification"));
+app.use("/student", require("./routes/student"));
+
+// No Route
+app.get("*", (req, res) => {
+  res.send({ Message: "No page found!" });
+});
+
+// Server and Database
+app.listen(8000, () => console.log(`Server started on port 8000`));
+mongoose.set("strictQuery", true);
+mongoose.connect(
+  process.env.MDB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
