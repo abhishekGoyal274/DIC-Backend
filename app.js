e1ed7de7ed145fa72aadffdf63bace868d183de6
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-07-02 11:59:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("*", (req, res) => {
 });
 
 // Server and Database
-app.listen(8000, () => console.log(`Server started on port 8000`));
+app.listen(process.env.PORT || 8000, () => console.log(`Server started on port 8000`));
 mongoose.set("strictQuery", true);
 mongoose.connect(
   process.env.MDB_CONNECT,
